cb0ba04295d56f5d5714812d8f22d36a73a5ca81
Mike-Jeong <alstlr65@gmail.com>
2023-01-13 01:03:41 +0900

    12012023

--- src/backjoon/b12012023.java
+++ src/backjoon/b12012023.java
@@ -3,92 +3,52 @@ package backjoon;
 import java.io.*;
 import java.util.*;
 
-//웜홀
+//운동
 //12012023
 
 public class b12012023 {
 
-    static ArrayList<int[]>[] edges;
-    static long[] dp;
-
-    private static final long INF = Long.MAX_VALUE;
-
+    private static int minVal = 987654321;
+    
     public static void main(String args[]) throws Exception {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int v = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
 
-        int tc = Integer.parseInt(st.nextToken());
+        int[][] map = new int[v + 1][v + 1];
 
-        for (int z = 0; z < tc; z++) {
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map.length; j++) {
+                map[i][j] = minVal;
+            }
+        }
+
+        for (int i = 0; i < e; i++) {
             st = new StringTokenizer(br.readLine());
 
-            int n = Integer.parseInt(st.nextToken());
-            int m = Integer.parseInt(st.nextToken());
-            int w = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
 
-            edges = new ArrayList[n + 1];
-            dp = new long[n + 1];
-
-            for (int i = 1; i <= n; i++) {
-                edges[i] = new ArrayList<>();
-            }
-
-            for (int i = 0; i < m; i++) {
-                st = new StringTokenizer(br.readLine());
-
-                int s = Integer.parseInt(st.nextToken());
-                int e = Integer.parseInt(st.nextToken());
-                int t = Integer.parseInt(st.nextToken());
-
-                edges[s].add(new int[] { e, t });
-                edges[e].add(new int[] { s, t });
-            }
-
-            for (int i = 0; i < w; i++) {
-                st = new StringTokenizer(br.readLine());
-
-                int s = Integer.parseInt(st.nextToken());
-                int e = Integer.parseInt(st.nextToken());
-                int t = Integer.parseInt(st.nextToken());
-
-                edges[s].add(new int[] { e, -t });
-            }
-
-            solution(1);
+            map[a][b] = c;
         }
 
-    }
-
-    // 벨만-포드 알고리즘
-    private static void solution(int start) {
-
-        boolean isUpdate = false;
-
-        Arrays.fill(dp, INF);
-
-        dp[start] = 0;
-
-        // 정점의 갯수만큼(V)
-        for (int i = 1; i < dp.length; i++) {
-
-            isUpdate = false;
-
-            for (int j = 1; j < dp.length; j++) {
-                for (int[] ed : edges[j]) {
-                    if (dp[j] != INF && dp[ed[0]] > dp[j] + ed[1]) {
-                        dp[ed[0]] = dp[j] + ed[1];
-                        isUpdate = true;
-                    }
+        for (int i = 1; i <= v; i++) {
+            for (int j = 1; j <= v; j++) {
+                for (int j2 = 1; j2 <= v; j2++) {
+                    map[j][j2] = Math.min(map[j][j2], map[j][i] + map[i][j2]);
                 }
             }
-
-            if (!isUpdate) {
-                break;
-            }
         }
 
-        System.out.println(isUpdate ? "YES" : "NO");
+        for (int i = 1; i <= v; i++) {
+            minVal = Math.min(minVal, map[i][i]);
+        }
+
+        System.out.println(minVal == 987654321 ? -1 : minVal);
 
     }
 }
