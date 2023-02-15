765473b52277d24e2455a2704ea3ba226a1bb512
Mike-Jeong <alstlr65@gmail.com>
2023-02-15 21:29:44 +0900

    15022023

---
+++ src/backjoon/b15022023.java
@@ -0,0 +1,79 @@
+package backjoon;
+
+import java.io.*;
+import java.util.*;
+
+//서강그라운드
+//15022023
+
+public class b15022023 {
+
+    static int[] items;
+    static int[][] map;
+
+    private static final int INF = 987654321;
+
+    public static void main(String args[]) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        // StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+
+        items = new int[n + 1];
+        map = new int[n + 1][n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == j) {
+                    continue;
+                }
+                map[i][j] = INF;
+            }
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            items[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < r; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+
+            map[s][e] = w;
+            map[e][s] = w;
+
+        }
+
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if (map[i][j] > map[i][k] + map[k][j]) {
+                        map[i][j] = map[i][k] + map[k][j];
+                    }
+                }
+            }
+        }
+
+        int ans = 0;
+        for (int i = 1; i <= n; i++) {
+            int count = 0;
+            for (int j = 1; j <= n; j++) {
+                if (map[i][j] <= m) {
+                    count += items[j];
+                }
+            }
+            ans = Math.max(ans, count);
+        }
+
+        System.out.println(ans);
+
+    }
+}
