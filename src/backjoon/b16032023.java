4cc2dbba850c11547c58e5766d26fc8a9f01509b
Mike-Jeong <alstlr65@gmail.com>
2023-03-16 10:50:37 +0900

    16032023

---
+++ src/backjoon/b16032023.java
@@ -0,0 +1,50 @@
+package backjoon;
+
+import java.io.*;
+import java.util.*;
+
+//좌표 정렬하기
+//16032023
+
+public class b16032023 {
+
+    public static void main(String args[]) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        // StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(st.nextToken());
+
+        int[][] positions = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+
+            st = new StringTokenizer(br.readLine());
+
+            positions[i][0] = Integer.parseInt(st.nextToken());
+            positions[i][1] = Integer.parseInt(st.nextToken());
+
+        }
+
+        Arrays.sort(positions, new Comparator<int[]>() {
+
+            @Override
+            public int compare(int[] o1, int[] o2) {
+
+                if (o1[0] == o2[0]) {
+                    return o1[1] - o2[1];
+                }
+
+                return o1[0] - o2[0];
+            }
+
+        });
+
+        for (int i = 0; i < n; i++) {
+
+            System.out.println(positions[i][0] + " " + positions[i][1]);
+
+        }
+    }
+}
