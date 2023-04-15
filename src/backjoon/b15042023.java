fda76ab6472f04d571110a18b5e442ba161e09c3
Mike-Jeong <alstlr65@gmail.com>
2023-04-15 23:46:57 +0900

    15042023

---
+++ src/backjoon/b15042023.java
@@ -0,0 +1,37 @@
+package backjoon;
+
+import java.io.*;
+import java.util.*;
+
+//수 정렬하기 4
+//15042023
+
+public class b15042023 {
+
+    public static void main(String args[]) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(st.nextToken());
+
+        int[] nums = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            nums[i] = Integer.parseInt(st.nextToken());
+
+        }
+
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length; i++) {
+            sb.append(nums[n - i - 1]).append("\n");
+        }
+
+        System.out.println(sb);
+
+    }
+}
