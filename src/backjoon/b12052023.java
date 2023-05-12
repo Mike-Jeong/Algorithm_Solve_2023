a22a854328f02d8426a59149077e00a900fc3761
Mike-Jeong <alstlr65@gmail.com>
2023-05-12 23:13:35 +0900

    12052023

---
+++ src/backjoon/b12052023.java
@@ -0,0 +1,22 @@
+package backjoon;
+
+import java.io.*;
+import java.util.*;
+
+//단어 길이 재기
+//12052023
+
+public class b12052023 {
+
+    public static void main(String args[]) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        // StringBuilder sb = new StringBuilder();
+
+        String s = st.nextToken();
+
+        System.out.println(s.length());
+
+    }
+}
