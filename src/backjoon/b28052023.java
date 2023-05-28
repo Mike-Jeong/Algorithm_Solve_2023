dec351c52783a10538f3703820033783a71b523e
Mike-Jeong <alstlr65@gmail.com>
2023-05-28 22:18:27 +0900

    28052023

---
+++ src/backjoon/b28052023.java
@@ -0,0 +1,26 @@
+package backjoon;
+
+import java.io.*;
+//import java.util.*;
+
+//그대로 출력하기
+//28052023
+
+public class b28052023 {
+
+    public static void main(String args[]) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //StringTokenizer st = new StringTokenizer(br.readLine());
+        //StringBuilder sb = new StringBuilder();
+
+        String s = "";
+
+        while ((s = br.readLine()) != null) {
+
+            System.out.println(s);
+
+            
+        }
+    }
+}
