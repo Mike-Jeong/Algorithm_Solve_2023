21e1980a58b99a758c1f04dc39644f734b838876
Mike-Jeong <alstlr65@gmail.com>
2023-07-07 23:23:20 +0900

    07072023

---
+++ src/backjoon/b07072023.java
@@ -0,0 +1,32 @@
+package backjoon;
+
+import java.io.*;
+// import java.util.*;
+
+//if
+//07072023
+
+public class b07072023 {
+
+    public static void main(String[] args) throws IOException {
+        // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // StringTokenizer st = new StringTokenizer(br.readLine());
+        // StringBuilder sb = new StringBuilder();
+
+//         import java.util.*;
+//         public class Main {
+//             public static void main(String[] args) {
+//                 ??? x = ???;
+//                 if (x != 0 && x == -x) {
+//                     System.out.println("true");
+//                 } else {
+//                     System.out.println("false");
+//                 }
+//             }
+//         }
+
+//         답 : int -2147483648
+
+    }
+
+}
