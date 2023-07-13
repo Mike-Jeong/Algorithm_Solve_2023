a10f7921424c10f4618c6e306975ef47d4f22323
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2023-07-13 21:20:40 +0900

    13072023

---
+++ src/backjoon/b13072023.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+//귀걸이
+//13072023
+
+public class b13072023 {
+    public static void main(String args[]) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int count = 1;
+        while (true) {
+            st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+
+            if (n == 0) {
+                System.out.println(sb);
+                return;
+            }
+
+            String[] names = new String[n];
+            for (int i = 0; i < n; i++) {
+                names[i] = br.readLine();
+            }
+
+            int sum = (n * (n + 1));
+            for (int i = 0; i < (2 * n) - 1; i++) {
+                st = new StringTokenizer(br.readLine());
+                int idx = Integer.parseInt(st.nextToken());
+                sum -= idx;
+            }
+
+            sb.append(count).append(" ").append(names[sum - 1]).append("\n");
+            count++;
+
+        }
+    }
+
+}
