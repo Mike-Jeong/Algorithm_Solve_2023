2ca7af2e04a06593e12e13c68aaf14a16cd86d33
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2023-07-26 18:58:30 +0900

    26072023

---
+++ src/backjoon/b26072023.java
@@ -0,0 +1,29 @@
+package backjoon;
+
+import java.io.*;
+import java.util.*;
+
+//X보다 작은 수
+//25072023
+
+public class b25072023 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+      
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            if (m > num) {
+              sb.append(num).append("\n");
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
