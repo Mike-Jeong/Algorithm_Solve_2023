3aeee60e2de57535d079ea581e66e2a48890f0a0
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2023-07-31 22:34:51 +0900

    31072023

---
+++ src/backjoon/b31072023.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+//??!
+//31072023
+
+public class Main {
+
+    public static void main(String args[]) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        // StringBuilder sb = new StringBuilder();
+
+        System.out.println(st.nextToken() + "??!");
+
+    }
+}
