491905106fb3bdc0a65585276a4c15c9be134b1b
Mike-Jeong <alstlr65@gmail.com>
2023-08-15 20:19:31 +0900

    15082023

---
+++ src/backjoon/b15082023.java
@@ -0,0 +1,24 @@
+package backjoon;
+
+// import java.io.*;
+// import java.util.*;
+
+//마이크로소프트 로고
+//15082023
+
+public class b15082023 {
+
+    public static void main(String args[]) throws Exception {
+
+        // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // StringTokenizer st = new StringTokenizer(br.readLine());
+        // StringBuilder sb = new StringBuilder();
+
+        System.out.println("       _.-;;-._");
+        System.out.println("'-..-'|   ||   |");
+        System.out.println("'-..-'|_.-;;-._|");
+        System.out.println("'-..-'|   ||   |");
+        System.out.println("'-..-'|_.-''-._|");
+
+    }
+}
