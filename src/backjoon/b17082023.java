4cff6fd0513d62bd997af264bd8de5b1ca3892bc
Mike-Jeong <alstlr65@gmail.com>
2023-08-17 18:35:08 +0900

    17082023

---
+++ src/backjoon/b17082023.java
@@ -0,0 +1,19 @@
+package backjoon;
+
+// import java.io.*;
+// import java.util.*;
+
+//고려대는 사랑입니다
+//17082023
+
+public class b17082023 {
+
+    public static void main(String args[]) throws Exception {
+
+        // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // StringTokenizer st = new StringTokenizer(br.readLine());
+        // StringBuilder sb = new StringBuilder();
+
+        System.out.println("고려대학교");
+    }
+}
