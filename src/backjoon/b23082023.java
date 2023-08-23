db19d9238aa78a9ec61b06c048a636c71df62615
Mike-Jeong <alstlr65@gmail.com>
2023-08-23 20:48:40 +0900

    23082023

---
+++ src/backjoon/b23082023.java
@@ -0,0 +1,29 @@
+package backjoon;
+
+// import java.io.*;
+// import java.util.*;
+
+//스타워즈 로고
+//23082023
+
+public class b23082023 {
+
+    public static void main(String args[]) throws Exception {
+
+        // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // StringTokenizer st = new StringTokenizer(br.readLine());
+        // StringBuilder sb = new StringBuilder();
+
+        System.out.print("    8888888888  888    88888\n" +
+                "   88     88   88 88   88  88\n" +
+                "    8888  88  88   88  88888\n" +
+                "       88 88 888888888 88   88\n" +
+                "88888888  88 88     88 88    888888\n" +
+                "\n" +
+                "88  88  88   888    88888    888888\n" +
+                "88  88  88  88 88   88  88  88\n" +
+                "88 8888 88 88   88  88888    8888\n" +
+                " 888  888 888888888 88  88      88\n" +
+                "  88  88  88     88 88   88888888");
+    }
+}
