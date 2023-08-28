566d3275e8c78bd9d441d67b7892b99287103bef
Mike-Jeong <alstlr65@gmail.com>
2023-08-28 23:08:27 +0900

    28082023

---
+++ src/backjoon/b28082023.java
@@ -0,0 +1,20 @@
+package backjoon;
+
+// import java.io.*;
+// import java.util.*;
+
+//새로운 시작
+//28082023
+
+public class b28082023 {
+
+    public static void main(String args[]) throws Exception {
+
+        // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // StringTokenizer st = new StringTokenizer(br.readLine());
+        // StringBuilder sb = new StringBuilder();
+
+        
+        System.out.println("파이팅!!");
+    }
+}
