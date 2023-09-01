0da29fd08b0f13a2827cd8d15fb6710b63d3fd8f
Mike-Jeong <alstlr65@gmail.com>
2023-09-01 22:27:33 +0900

    01092023

---
+++ src/backjoon/b01092023.java
@@ -0,0 +1,22 @@
+package backjoon;
+
+import java.io.*;
+import java.util.*;
+
+//CAPS
+//01092023
+
+public class b01092023 {
+
+    public static void main(String args[]) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        // StringBuilder sb = new StringBuilder();
+
+        String s = st.nextToken();
+
+        System.out.println(s.toUpperCase());
+
+    }
+}
