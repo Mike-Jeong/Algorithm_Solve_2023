cdb3df93c4e969b70d50e9d0c8a2339ff305ad90
Mike-Jeong <alstlr65@gmail.com>
2023-09-10 20:03:50 +0900

    10092023

---
+++ src/backjoon/b10092023.java
@@ -0,0 +1,25 @@
+package backjoon;
+
+// import java.io.*;
+// import java.util.*;
+
+//나부 함대 데이터
+//10092023
+
+public class b10092023 {
+
+    public static void main(String args[]) throws Exception {
+
+        // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // StringTokenizer st = new StringTokenizer(br.readLine());
+        // StringBuilder sb = new StringBuilder();
+
+        System.out.println("SHIP NAME      CLASS          DEPLOYMENT IN SERVICE");
+        System.out.println("N2 Bomber      Heavy Fighter  Limited    21        ");
+        System.out.println("J-Type 327     Light Combat   Unlimited  1         ");
+        System.out.println("NX Cruiser     Medium Fighter Limited    18        ");
+        System.out.println("N1 Starfighter Medium Fighter Unlimited  25        ");
+        System.out.println("Royal Cruiser  Light Combat   Limited    4         ");
+
+    }
+}
