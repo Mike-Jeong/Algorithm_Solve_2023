ca7f34b9825d41996a76cd606726a6d4e35d1d9b
Mike-Jeong <alstlr65@gmail.com>
2023-09-12 19:01:38 +0900

    12092023

---
+++ src/backjoon/b12092023.java
@@ -0,0 +1,22 @@
+package backjoon;
+
+import java.io.*;
+import java.util.*;
+
+//UCPC에서 가장 쉬운 문제 번호는?
+//12092023
+
+public class b12092023 {
+
+    public static void main(String args[]) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        // StringBuilder sb = new StringBuilder();
+
+        st.nextToken();
+
+        System.out.println("A");
+
+    }
+}
