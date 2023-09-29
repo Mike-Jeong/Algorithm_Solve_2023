100d7e1540d19e9263cbe925a50e2f8d92b8ae5f
Mike-Jeong <alstlr65@gmail.com>
2023-09-29 11:40:12 +0900

    29092023

---
+++ src/backjoon/b29092023.java
@@ -0,0 +1,19 @@
+package backjoon;
+
+// import java.io.*;
+// import java.util.*;
+
+//와이버스 부릉부릉
+//28092023
+
+public class b29092023 {
+
+    public static void main(String args[]) throws Exception {
+
+        // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // StringTokenizer st; // = new StringTokenizer(br.readLine());
+        // StringBuilder sb = new StringBuilder();
+
+        System.out.println("비와이");
+    }
+}
