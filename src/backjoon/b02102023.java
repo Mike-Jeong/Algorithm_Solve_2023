a6ed2f74e2b2fbd542010b46c738d4e61b3cd1ed
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2023-10-02 18:10:39 +0900

    02102023

---
+++ src/backjoon/b02102023.java
@@ -0,0 +1,21 @@
+package backjoon;
+
+// import java.io.*;
+// import java.util.*;
+
+//알고리즘 수업 - 알고리즘의 수행 시간 1 성공
+//02102023
+
+public class b02102023 {
+
+    public static void main(String args[]) throws Exception {
+
+        // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // StringTokenizer st = new StringTokenizer(br.readLine());
+        // StringBuilder sb = new StringBuilder();
+
+        System.out.println(1);
+        System.out.println(0);
+
+    }
+}
