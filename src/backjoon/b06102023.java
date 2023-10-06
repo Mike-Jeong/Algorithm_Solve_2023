0234f16bdaba7f467920e5d620103b9917d9c87d
Mike-Jeong <alstlr65@gmail.com>
2023-10-06 23:28:06 +0900

    06102023

---
+++ src/backjoon/b06102023.java
@@ -0,0 +1,20 @@
+package backjoon;
+
+// import java.io.*;
+// import java.util.*;
+
+//
+//06102023
+
+public class b06102023 {
+
+    public static void main(String args[]) throws Exception {
+
+        // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // StringTokenizer st = new StringTokenizer(br.readLine());
+        // StringBuilder sb = new StringBuilder();
+
+        System.out.println("The world says hello!");
+
+    }
+}
