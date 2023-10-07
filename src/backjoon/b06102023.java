35a9e1f8e4ce62353d2b9f939215ca6478220b8a
Mike-Jeong <alstlr65@gmail.com>
2023-10-07 13:59:01 +0900

    07102023

--- src/backjoon/b06102023.java
+++ src/backjoon/b06102023.java
@@ -1,20 +1,24 @@
 package backjoon;
 
-// import java.io.*;
-// import java.util.*;
+import java.io.*;
+import java.util.*;
 
-//
+//Rats
 //06102023
 
 public class b06102023 {
 
     public static void main(String args[]) throws Exception {
 
-        // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        // StringTokenizer st = new StringTokenizer(br.readLine());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
         // StringBuilder sb = new StringBuilder();
 
-        System.out.println("The world says hello!");
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int x = Integer.parseInt(st.nextToken());
+
+        System.out.println((((n + 1) * (m + 1)) / (x + 1)) - 1);
 
     }
 }
---
+++ src/backjoon/b07102023.java
@@ -0,0 +1,20 @@
+package backjoon;
+
+// import java.io.*;
+// import java.util.*;
+
+//The World Responds
+//07102023
+
+public class b07102023 {
+
+    public static void main(String args[]) throws Exception {
+
+        // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // StringTokenizer st = new StringTokenizer(br.readLine());
+        // StringBuilder sb = new StringBuilder();
+
+        System.out.println("The world says hello!");
+
+    }
+}
